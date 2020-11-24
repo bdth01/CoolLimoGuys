13ff1e0a4523eba6f07ca1bf6a4220d2d50a0baf
bob <tayler.kleinow@nwktc.edu>
2020-11-24 08:10:47 -0700

    java stuff

--- script.js
+++ script.js
@@ -122,4 +122,31 @@
 //   total.innerHTML = `$${tipValue}`
 // })
 // })
-// >>>>>>> 160b6006d499d0d3d3da7c0fb5a1a37f722df897
+//Timer stuff *-----------------------------------------------------------------
+var countDownDate = new Date("Nov 27, 2020 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("homeTimer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("homeTimer").innerHTML = "EXPIRED";
+  }
+}, 1000);
